51e110a964a79e749cb78bd4332c2f5cc629895e
Eduardo Matheus de Menezes Souto <eduardomatheusuesc@gmail.com>
2024-09-30 13:14:17 -0300

    feat/DashboardScreen Terminando a tela de DashBoard

---
+++ expoAuthTs/src/hooks/useUserData.ts
@@ -0,0 +1,35 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect, useState } from "react";
+
+interface UserData {
+    email: string;
+    lastLogin: string;
+}
+
+export const useUserData = () => {
+    const [userData, setUserData] = useState<UserData | null>(null);
+    const  [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const loadUserData = async () => {
+            try {
+                const storedUser = await AsyncStorage.getItem("user");
+                if (storedUser) {
+                    const parsedUser = JSON.parse(storedUser);
+                    setUserData({
+                        email: parsedUser.email,
+                        lastLogin: new Date().toLocaleString(),
+                    });
+
+                }
+            } catch (error) {
+                alert(`Erro ao carregar dados do usúario, ${error}`);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        loadUserData();
+    }, []);
+    return { userData, loading };
+};
